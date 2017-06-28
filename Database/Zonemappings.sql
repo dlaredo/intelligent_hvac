55cf839070232d4a97b82c91661099e192519480
spndo <viviancday@gmail.com>
2017-06-28 12:18:48 -0700

    Update Zonemappings.sql

--- Database/Zonemappings.sql
+++ Database/Zonemappings.sql
@@ -2,7 +2,7 @@ insert into PathMappings (Path, ComponentType, Description, DatabaseMapping) val
 insert into PathMappings (Path, ComponentType, Description, DatabaseMapping) values ("m135", "Thermafuser Sensor", "OccupiedCoolingSetpoint", "Thermafuser_Reading.OccupiedCoolingSetpoint")
 insert into PathMappings (Path, ComponentType, Description, DatabaseMapping) values ("m134", "Thermafuser Sensor", "OccupiedHeatingSetpoint", "Thermafuser_Reading.OccupiedHeatingSetpoint")
 insert into PathMappings (Path, ComponentType, Description, DatabaseMapping) values ("m078", "Thermafuser Sensor", "RoomOccupied", "Thermafuser_Reading.RoomOccupied")
-insert into PathMappings (Path, ComponentType, Description, DatabaseMapping) values ("m190", "Thermafuser Sensor", "ZoneTemperature", "Thermafuser_Reading.ZoneTemperature")
-insert into PathMappings (Path, ComponentType, Description, DatabaseMapping) values ("m186", "Thermafuser Sensor", "SupplyAir", "Thermafuser_Reading.SupplyAir")
+insert into PathMappings (Path, ComponentType, Description, DatabaseMapping) values ("m190", "Thermafuser Sensor", "SupplyAir", "Thermafuser_Reading.SupplyAir")
+insert into PathMappings (Path, ComponentType, Description, DatabaseMapping) values ("m186", "Thermafuser Sensor", "ZoneTemperature", "Thermafuser_Reading.ZoneTemperature")
 
 insert into PathMappings (Path, ComponentType, Description, DatabaseMapping) values ("chw_valve", "Heat Exchanger Coil", "Chilled Water Valve", "Heat_Exchanger_Coil.WaterTemperature")
