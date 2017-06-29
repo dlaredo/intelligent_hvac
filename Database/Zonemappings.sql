521c23bb1dedcf3bdec148894861efc4a6654640
Vivian Chen <viviancday@gmail.com>
2017-06-29 11:03:38 -0700

    changed space between words

--- Database/Zonemappings.sql
+++ Database/Zonemappings.sql
@@ -1,11 +1,11 @@
 
 #Thermafuser
 insert into PathMappings (Path, ComponentType, Description, DatabaseMapping) values ("m073", "Thermafuser Sensor", "Airflow Feedback", "Thermafuser_Reading.AirflowFeedback")
-insert into PathMappings (Path, ComponentType, Description, DatabaseMapping) values ("m135", "Thermafuser Sensor", "OccupiedCoolingSetpoint", "Thermafuser_Reading.OccupiedCoolingSetpoint")
-insert into PathMappings (Path, ComponentType, Description, DatabaseMapping) values ("m134", "Thermafuser Sensor", "OccupiedHeatingSetpoint", "Thermafuser_Reading.OccupiedHeatingSetpoint")
-insert into PathMappings (Path, ComponentType, Description, DatabaseMapping) values ("m078", "Thermafuser Sensor", "RoomOccupied", "Thermafuser_Reading.RoomOccupied")
-insert into PathMappings (Path, ComponentType, Description, DatabaseMapping) values ("m190", "Thermafuser Sensor", "SupplyAir", "Thermafuser_Reading.SupplyAir")
-insert into PathMappings (Path, ComponentType, Description, DatabaseMapping) values ("m186", "Thermafuser Sensor", "ZoneTemperature", "Thermafuser_Reading.ZoneTemperature")
+insert into PathMappings (Path, ComponentType, Description, DatabaseMapping) values ("m135", "Thermafuser Sensor", "Occupied Cooling Setpoint", "Thermafuser_Reading.OccupiedCoolingSetpoint")
+insert into PathMappings (Path, ComponentType, Description, DatabaseMapping) values ("m134", "Thermafuser Sensor", "Occupied Heating Setpoint", "Thermafuser_Reading.OccupiedHeatingSetpoint")
+insert into PathMappings (Path, ComponentType, Description, DatabaseMapping) values ("m078", "Thermafuser Sensor", "Room Occupied", "Thermafuser_Reading.RoomOccupied")
+insert into PathMappings (Path, ComponentType, Description, DatabaseMapping) values ("m190", "Thermafuser Sensor", "Supply Air", "Thermafuser_Reading.SupplyAir")
+insert into PathMappings (Path, ComponentType, Description, DatabaseMapping) values ("m186", "Thermafuser Sensor", "Zone Temperature", "Thermafuser_Reading.ZoneTemperature")
 
 #Heat Exchanger Coil
 insert into PathMappings (Path, ComponentType, Description, DatabaseMapping) values ("cw_valve", "Heat Exchanger Coil", "Chilled Water Valve Opening", "Heat_Exchanger_Coil.ValveOpeningPercentage")
