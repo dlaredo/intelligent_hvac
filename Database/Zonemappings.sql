b71bcc2e819dfa42fc189adc790e9b1940a679d4
dlaredo <davidlaredo1@gmail.com>
2017-06-28 19:16:20 -0500

    Adding Vivians changes

--- Database/Zonemappings.sql
+++ Database/Zonemappings.sql
@@ -36,3 +36,23 @@ insert into PathMappings (Path, ComponentType, Description, DatabaseMapping) val
 insert into PathMappings (Path, ComponentType, Description, DatabaseMapping) values ("ra_co2", "AHU", "Return Air Co2", "Air_Handling_Unit_Reading.ReturnAirCO2")
 insert into PathMappings (Path, ComponentType, Description, DatabaseMapping) values ("ra_temp", "AHU", "Return Air Temperature", "Air_Handling_Unit_Reading.ReturnAirTemperature")
 insert into PathMappings (Path, ComponentType, Description, DatabaseMapping) values ("m798", "AHU", "Static Pressure Smoothed", "Air_Handling_Unit_Reading.StaticPressureSmoothed")
+
+
+#vav
+#missing -> HW/CW Setpt/Valve
+insert into PathMappings (Path, ComponentType, Description, DatabaseMapping) values ("da_temp", "Variable Air Volume", "Discharge Temperature", "Variable_Air_Volume_Reading.DischargeTemperature")
+insert into PathMappings (Path, ComponentType, Description, DatabaseMapping) values ("dmpr_pos_tn", "Variable Air Volume", "Damper Position", "Variable_Air_Volume_Reading.DamperPosition")
+insert into PathMappings (Path, ComponentType, Description, DatabaseMapping) values ("st_press", "Variable Air Volume", "Duct Static Pressure", "Variable_Air_Volume_Reading.DuctStaticPressure")
+insert into PathMappings (Path, ComponentType, Description, DatabaseMapping) values ("flow_input", "Variable Air Volume", "Flow Control/Flow Input", "Variable_Air_Volume_Reading.WaterTemperature")
+insert into PathMappings (Path, ComponentType, Description, DatabaseMapping) values ("m311", "Variable Air Volume", "Zone Temperature", "Variable_Air_Volume_Reading.ZoneTemperature")
+insert into PathMappings (Path, ComponentType, Description, DatabaseMapping) values ("m211", "Variable Air Volume", "Static Setpoint 1", "Variable_Air_Volume_Reading.DuctStaticPressure")
+
+#bridge
+insert into PathMappings (Path, ComponentType, Description, DatabaseMapping) values ("m073", "Bridge Thermafuser Sensor", "Airflow Feedback", "Thermafuser_Reading.AirflowFeedback")
+insert into PathMappings (Path, ComponentType, Description, DatabaseMapping) values ("m135", "Bridge Thermafuser Sensor", "Occupied Cooling Setpoint", "Thermafuser_Reading.OccupiedCoolingSetpoint")
+insert into PathMappings (Path, ComponentType, Description, DatabaseMapping) values ("m134", "Bridge Thermafuser Sensor", "Occupied Heating Setpoint", "Thermafuser_Reading.OccupiedHeatingSetpoint")
+insert into PathMappings (Path, ComponentType, Description, DatabaseMapping) values ("m078", "Bridge Thermafuser Sensor", "Room Occupied", "Thermafuser_Reading.RoomOccupied")
+insert into PathMappings (Path, ComponentType, Description, DatabaseMapping) values ("m190", "Bridge Thermafuser Sensor", "Supply Air", "Thermafuser_Reading.SupplyAir")
+insert into PathMappings (Path, ComponentType, Description, DatabaseMapping) values ("m186", "Bridge Thermafuser Sensor", "Zone Temperature", "Thermafuser_Reading.ZoneTemperature")
+
+
