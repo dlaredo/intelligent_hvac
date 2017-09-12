873f4c043d6579ae86a41cc664226529f65f30be
Unknown <dlaredorazo@ucmerced.edu>
2017-09-12 13:48:24 -0500

    Script for clearing database

---
+++ Database/cleanReadings.sql
@@ -0,0 +1,11 @@
+use hvacinternettest;
+
+truncate air_handling_unit_reading;
+truncate damper_reading;
+truncate fan_reading;
+truncate filter_reading;
+truncate heat_exchanger_coil_reading;
+truncate staged_air_volume_reading;
+truncate thermafuser_reading;
+truncate variable_air_volume_reading;
+truncate vfd_reading;
